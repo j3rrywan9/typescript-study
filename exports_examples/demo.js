cb1f72c377518f8de3fe6e9835b2262d5291d6de
Jerry Wang <j3rry.wan9@gmail.com>
2016-12-09 22:40:25 -0800

    add exports_examples/parameter.js

--- exports_examples/demo.js
+++ exports_examples/demo.js
@@ -1,3 +1,5 @@
+// node --use_strict demo.js
+
 const Point = require('./Point.js');
 
 var p1 = new Point(1, 2);
@@ -5,3 +7,8 @@ console.log(p1);
 
 var p2 = new Point(3, 4);
 console.log(p2);
+
+const jerry = require('./parameter.js')('Jerry');
+console.log(jerry);
+console.log(jerry.name);
+console.log(jerry.sayHi());
